216343fe88b2a75df10761ed782933e0d07f146c
João Victor <reinoldesxbox@gmail.com>
2024-05-25 14:35:38 -0400

    Adicionar arquivos de projeto.

---
+++ BankAccountProject/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+BankAccount bankaccount1 = new BankAccount();
+bankaccount1.accountid = 25446;
+bankaccount1.name = "Joao Victor";
+bankaccount1.balance = 50.28;
+bankaccount1.password = "password";
+
+BankAccount bankaccount2 = new BankAccount();
+bankaccount2.accountid = 24785;
+bankaccount2.name = "Giulia";
+bankaccount2.balance = 205768633.05;
+bankaccount2.password = "loveyou";
+
+bankaccount1.BankInformation();
+bankaccount2.BankInformation();
---
+++ BankAccountProject/bankaccountclass.cs
@@ -0,0 +1,13 @@
+﻿class BankAccount
+{
+    public int accountid;
+    public string name;
+    public double balance;
+    public string password;
+
+
+    public void BankInformation()
+    {
+        Console.WriteLine($"Hello {name}, your account ID is {accountid} and your current balance is ${balance}, in case you forgot your password here it is{password}. Hope you have a nice week!\n");
+    }
+}
