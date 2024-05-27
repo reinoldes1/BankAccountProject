995ddd717c12b397b5950aff11434f8189c49ed8
João Victor <reinoldesxbox@gmail.com>
2024-05-27 14:32:20 -0400

    Updating code with properties and adding Deposit and withdrawl functions which affect balance.

--- BankAccountProject/Program.cs
+++ BankAccountProject/Program.cs
@@ -1,16 +1,24 @@
-﻿
+﻿using System;
 
-BankAccount bankaccount1 = new BankAccount();
-bankaccount1.accountid = 25446;
-bankaccount1.name = "Joao Victor";
-bankaccount1.balance = 50.28;
-bankaccount1.password = "password";
+class Program
+{
+    static void Main()
+    {
+        BankAccount bankaccount1 = new BankAccount();
+        bankaccount1.Accountid = 25446;
+        bankaccount1.Name = "Joao Victor";
+        bankaccount1.Deposit = 2500.57;
+        bankaccount1.Withdraw = 50.28;
+        bankaccount1.Password = "password";
 
-BankAccount bankaccount2 = new BankAccount();
-bankaccount2.accountid = 24785;
-bankaccount2.name = "Giulia";
-bankaccount2.balance = 205768633.05;
-bankaccount2.password = "loveyou";
+        BankAccount bankaccount2 = new BankAccount();
+        bankaccount2.Accountid = 24785;
+        bankaccount2.Name = "Giulia";
+        bankaccount2.Deposit = 15350.44;
+        bankaccount2.Withdraw = 8633.05;
+        bankaccount2.Password = "loveyou";
 
-bankaccount1.BankInformation();
-bankaccount2.BankInformation();
+        bankaccount1.BankInformation();
+        bankaccount2.BankInformation();
+    }
+}
--- BankAccountProject/bankaccountclass.cs
+++ BankAccountProject/bankaccountclass.cs
@@ -1,13 +1,33 @@
 ﻿class BankAccount
 {
-    public int accountid;
-    public string name;
-    public double balance;
-    public string password;
+    public int Accountid { get; set; }
+    public string Name { get; set; }
+
+    public double Withdraw { get; set; }
+    public double Deposit { get; set; }
+
+    private double balance = 0;
+    public double  Balance
+
+    {
+        get
+        {
+            return Math.Round (balance + Deposit - Withdraw, 2);
+        }
+    }
+
+    public string Password {  get; set; }
 
 
     public void BankInformation()
     {
-        Console.WriteLine($"Hello {name}, your account ID is {accountid} and your current balance is ${balance}, in case you forgot your password here it is{password}. Hope you have a nice week!\n");
+        Console.WriteLine($"Hello {Name},\n" +
+                  $"Account ID: {Accountid}\n" +
+                  $"Current balance: ${Balance}\n" +
+                  $"In case you forgot your password, here it is: '{Password}'.\n" +
+                  "Hope you have a nice week!\n");
+
+        Console.WriteLine($"This week you deposited ${Deposit} and withdrawn ${Withdraw},\n" +
+                          $"which made your current balance ${Balance}\n");
     }
 }
